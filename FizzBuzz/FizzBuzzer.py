87267a997ab9b394662a735e79c4480241dc31c4
Roberto Monesi <roberto.monesi@outlook.com>
2020-12-28 16:20:58 +0000

    Implement Fizz case

--- FizzBuzz/FizzBuzzer.py
+++ FizzBuzz/FizzBuzzer.py
@@ -1,4 +1,7 @@
 
 def get_value(input: int) -> str:
     """Given an int it return Fizz, Buzz or FizzBuzz if it is a multiple of 3, 5 or both"""
-    return f'{input}'
+    result = f'{input}'
+    if input % 3 == 0:
+        result = 'Fizz'
+    return result
--- FizzBuzz/test_FizzBuzzer.py
+++ FizzBuzz/test_FizzBuzzer.py
@@ -11,3 +11,8 @@ STANDARD_NUMBERS = [i for i in range(0,100) if i not in FIZZBUZZ_NUMBERS and i n
 def test_get_value_return_standard_input(test_input: int) -> None:
     """When input are values not multiple of 3 nor 5 should return the input """
     assert FizzBuzzer.get_value(test_input) == f'{test_input}'
+
+@pytest.mark.parametrize("test_input", [num for num in FIZZ_NUMBERS])
+def test_get_vaue_should_return_Fizz_when_input_is_multiple_of_three(test_input: int) -> None:
+    """When input values are multiple of three should return fizz"""
+    assert FizzBuzzer.get_value(test_input) == 'Fizz'
