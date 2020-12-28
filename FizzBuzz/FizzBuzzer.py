3f6559c4f5d8cf958a4449c3938ed85fa1c25cf5
Roberto Monesi <roberto.monesi@outlook.com>
2020-12-28 16:14:58 +0000

    Implement first test and standard behavior

---
+++ FizzBuzz/FizzBuzzer.py
@@ -0,0 +1,4 @@
+
+def get_value(input: int) -> str:
+    """Given an int it return Fizz, Buzz or FizzBuzz if it is a multiple of 3, 5 or both"""
+    return f'{input}'
---
+++ FizzBuzz/test_FizzBuzzer.py
@@ -0,0 +1,13 @@
+import pytest
+import FizzBuzzer as FizzBuzzer
+
+
+FIZZBUZZ_NUMBERS = [i for i in range(0,100,15)]
+FIZZ_NUMBERS = [i for i in range(0,100, 3) if i not in FIZZBUZZ_NUMBERS]
+BUZZ_NUMBERS = [i for i in range(0,100, 5) if i not in FIZZBUZZ_NUMBERS]
+STANDARD_NUMBERS = [i for i in range(0,100) if i not in FIZZBUZZ_NUMBERS and i not in FIZZ_NUMBERS and i not in BUZZ_NUMBERS]
+
+@pytest.mark.parametrize("test_input", [num for num in STANDARD_NUMBERS])
+def test_get_value_return_standard_input(test_input: int) -> None:
+    """When input are values not multiple of 3 nor 5 should return the input """
+    assert FizzBuzzer.get_value(test_input) == f'{test_input}'
