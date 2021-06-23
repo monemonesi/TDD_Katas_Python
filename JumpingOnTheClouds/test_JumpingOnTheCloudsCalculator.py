ca99680670ead6b4ee96e97820c8f801b5343e41
Roberto Monesi <roberto.monesi@outlook.com>
2021-06-23 12:11:31 +0100

    clean-up

--- JumpingOnTheClouds/test_JumpingOnTheCloudsCalculator.py
+++ JumpingOnTheClouds/test_JumpingOnTheCloudsCalculator.py
@@ -13,15 +13,15 @@ def test_make_jump_should_increment_jumps_and_index_correctly(c, expected) -> No
     assert Calculator.make_jump(c, 0, 0) == expected
 
 
-def test_jumpingOnClouds_should_return_2_with_4_green_clouds() -> None:
+def test_jumping_on_clouds_should_return_2_with_4_green_clouds() -> None:
     """Test path with 4 greens clouds"""
-    assert Calculator.jumpingOnClouds([0, 0, 0, 0]) == 2
+    assert Calculator.jumping_on_clouds([0, 0, 0, 0]) == 2
 
 
-def test_jumpingOnClouds_return_the_correct_input_when_5_clouds() -> None:
-    assert Calculator.jumpingOnClouds([0, 0, 1, 0, 0]) == 3
+def test_jumping_on_clouds_return_the_correct_input_when_5_clouds() -> None:
+    assert Calculator.jumping_on_clouds([0, 0, 1, 0, 0]) == 3
 
 
 @pytest.mark.parametrize('path_result', [t for t in FIVE_CLOUDS_PATHS_RESULT])
-def test_jumpingOnClouds_return_the_correct_input_when_5_clouds(path_result: ()) -> None:
-    assert Calculator.jumpingOnClouds(path_result[0]) == path_result[1]
+def test_jumping_on_clouds_return_the_correct_input_when_5_clouds(path_result: ()) -> None:
+    assert Calculator.jumping_on_clouds(path_result[0]) == path_result[1]
