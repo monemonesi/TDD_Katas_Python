13bf49e644dbb7ab1f0525e7bfcf249f24416554
Roberto Monesi <roberto.monesi@outlook.com>
2022-10-10 06:35:59 +0100

    implement pair_sum algo

--- ArrayPairSum/pair_sum.py
+++ ArrayPairSum/pair_sum.py
@@ -1,2 +1,20 @@
-def pair_sum(numbers: [], sum: int) -> []:
-    return []
+def pair_sum(numbers: [], sum: int) -> set():
+    """
+    [1,3,2,2], 4
+    """
+    if len(numbers) < 2:
+        return []
+
+    output = set()
+    seen = set()
+
+    for num in numbers:
+        target = sum - num
+
+        if num not in seen:
+            seen.add(target)
+
+        else:
+            output.add((min(num, target), max(num, target)))
+
+    return output
